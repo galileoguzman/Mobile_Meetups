c80695e20e131c4ad9c2330224c2fc9c759789f3
Galileo Guzman <galileoguzman@gmail.com>
2017-08-31 02:40:27 -0500

    Add app navigation component

---
+++ AppNavigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export default class AppNavigation extends React.Component{
+    render(){
+        return (
+            <View>
+                <Text>App Navigation</Text>
+            </View>
+        )
+    }
+}
