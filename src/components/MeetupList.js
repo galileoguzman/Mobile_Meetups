8552d24d73545c2a20d88c4e834a1ebce3fa6994
Galileo Guzman <galileoguzman@gmail.com>
2017-08-31 02:09:54 -0500

    Added styles and Dimensions to calculate the width and height for each window.

--- src/components/MeetupList.js
+++ src/components/MeetupList.js
@@ -6,8 +6,11 @@ import {
     Text,
     Image,
     TouchableWithoutFeedback,
-    StyleSheet
+    StyleSheet,
+    Dimensions
 } from 'react-native';
+
+const {width, height} = Dimensions.get('window');
 
 export default class MeetupList extends React.Component {
     constructor(props){
@@ -19,13 +22,15 @@ export default class MeetupList extends React.Component {
 
     render(){
         return(
-            <ScrollView>
+            <ScrollView style={styles.container}>
                 {this.state.events.map((event, i) => (
-                    <TouchableWithoutFeedback>
-                        <View>
-                            <Image />
-                            <View>
-                                <Text>{event.title}</Text>
+                    <TouchableWithoutFeedback
+                        onPress={() => null}
+                    >
+                        <View style={styles.card}>
+                            <Image style={styles.image} source={{uri : event.groupImage}}/>
+                            <View style={styles.cardContent}>
+                                <Text style={styles.title}>{event.title}</Text>
                                 <Text>{event.groupName}</Text>
                             </View>
                         </View>
@@ -35,3 +40,34 @@ export default class MeetupList extends React.Component {
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        margin: 10,
+        height
+    },
+    card: {
+        backgroundColor: '#ddd',
+        borderColor: '#d9d9d9',
+        borderWidth: 1,
+        borderRadius: 3,
+        flex: 1,
+        flexDirection: 'row',
+        justify: 'space-between',
+        height: 100
+    },
+    image: {
+        height: 100,
+        width: 100
+    },
+    cardContent:{
+        flex: 1,
+        padding: 10,
+        margin: 0
+    },
+    titles: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    }
+})
