de2d4106eb8697e6c859ab72b0a572d051d5725f
Galileo Guzman <galileoguzman@gmail.com>
2017-09-04 18:55:54 -0500

    add styles to show logout button on navigation options

--- src/config/navOptions.js
+++ src/config/navOptions.js
@@ -1,4 +1,21 @@
+import React from 'react';
+import{
+    View,
+    Text,
+    Button
+} from 'react-native';
+
+import { auth } from './firebase';
+
 export const navigationOptions = {
+    headerRight: (
+        <View style={{ flexDirection: 'row', }}>
+            <Text style={{ color: 'white', paddingVertical: 5, paddingRight: 5 }}>
+                { auth.currentUser ? auth.currentUser.email : '' }
+            </Text>
+            <Button color='orangered' title='Salir' onPress={() => auth.signOut()}></Button>
+        </View>
+    ),
     headerStyle: {
         backgroundColor: '#4b5f83',
         paddingTop: 15,
